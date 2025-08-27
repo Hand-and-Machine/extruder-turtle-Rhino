68b9f4a35b6e2ff867de22a4eb8817569858def9
leahbuechley <buechley@gmail.com>
2025-08-27 13:29:47 -0600

    updated weave slicer examples

--- extruder_turtle/weave_slicer.py
+++ extruder_turtle/weave_slicer.py
@@ -8,6 +8,8 @@ import random
 import extruder_turtle
 from extruder_turtle import *
 
+
+# general purpose functions
 def slice_shape(shape, layer_height=1.0):
 	bounding_box = rs.BoundingBox(shape)
 	slice_vector=(bounding_box[0], bounding_box[4])
@@ -55,6 +57,160 @@ def slice_shape(shape, layer_height=1.0):
 		i+=1
 
 	return shape_slices_layer
+
+def get_offset_curves (curve,distance):
+
+	if (rs.IsCurveClosable(curve)==False):
+		print("Can't get offsets for an open form.")
+		return
+
+	point = rs.CurveAreaCentroid(curve)[0]
+	next_curve=True
+	count=0
+
+	curve_list=[]
+	curve_list=recursive_offset(curve,curve_list,distance)
+
+	new_curve_list=[]
+
+	#get rid of duplicate curves in list
+	for i in range(0,len(curve_list)):
+		flag=False
+		curve=curve_list[i]
+		start0 = rs.CurveStartPoint(curve)
+		for j in range(i+1,len(curve_list)):
+			start1 = rs.CurveStartPoint(curve_list[j])
+			if(rs.Distance(start0,start1)<distance/2.0):
+				flag=True
+				break
+
+		if (flag==False):
+			compare_point=[-1000,0,0]
+			closest=rs.CurveClosestPoint(curve_list[i], compare_point)
+			sort_point = rs.EvaluateCurve(curve_list[i], closest)
+			new_curve_list.append({"curve": curve_list[i], "x": sort_point.X})
+
+	new_curve_list.sort(key=lambda item: item["x"])
+
+	curves=[]
+	for i in range(len(new_curve_list)):
+		curves.append(new_curve_list[i]["curve"])
+
+	return curves
+
+def recursive_offset (curve,curve_list,distance):
+
+	if (rs.IsCurveClosable(curve)==False):
+		print("Can't make a bottom for an open form.")
+		return []
+
+	c=rs.CurveLength(curve)
+	point = rs.CurveAreaCentroid(curve)[0]
+
+	if (distance>c/10):
+		# print("offset end condition curve length")
+		return []
+
+	a = rs.CurveArea(curve)[0]
+	if (distance>math.sqrt(a/10)):
+		# print("offset end condition curve area")
+		return []
+
+	next_curve_list=rs.OffsetCurve(curve, point, distance=distance,style=3)
+	if (not(next_curve_list)):
+		# print("couldn't find any more offsets")
+		return []
+	try:
+		for i in range (0,len(next_curve_list)):
+			# add current offset curve to list:
+			# find new offsets for current offset:
+			curve_list = curve_list + [next_curve_list[i]]+recursive_offset(next_curve_list[i],curve_list,distance)
+	except Exception as e:
+		print(e)
+		print(len(next_curve_list))
+
+	return curve_list
+
+def follow_curve(t, curve, double_wall = False, inner_wall=False, steps=100):
+	points = rs.DivideCurve (curve, steps)
+	dtheta = 360.0/steps
+
+	t2=ExtruderTurtle()
+	t2.penup()
+
+	i=0
+	if (inner_wall==False):
+		for point in points:
+			if (i==0):
+				t.set_position(point.X,point.Y,point.Z)
+				t.pendown()
+				t2.set_position(point.X,point.Y,point.Z)
+				t2.pendown()
+			else:
+				t.pendown()
+				t2.pendown()
+			t.set_position(point.X,point.Y,point.Z)
+			t2.set_position(point.X,point.Y,point.Z)
+			i+=1
+
+	if (rs.IsCurveClosed(curve)==False): # if curve is not closed, lift pen
+			t.penup()
+			t2.penup()
+
+	t.set_position(points[0].X,points[0].Y,point.Z)
+	t.set_position(points[1].X,points[1].Y,point.Z)
+	t2.set_position(points[0].X,points[0].Y,point.Z)
+	t2.set_position(points[1].X,points[1].Y,point.Z)
+
+	if (double_wall or inner_wall):
+		inner_curve = rs.OffsetCurve(curve, [0,0,0], t.get_extrude_width())
+		print(inner_curve)
+		if (inner_curve):
+			inner_points = rs.DivideCurve (inner_curve, steps)
+		else:
+			#if offset fails, just return
+			print("Couldn't create an offset curve to follow.")
+			return()
+		i=0
+		for point in inner_points:
+			if (i==0):	
+				t.set_position(point.X,point.Y,point.Z)
+				t.pendown()
+				t2.set_position(point.X,point.Y,point.Z)
+				t2.pendown()
+			t.set_position(point.X,point.Y,point.Z)
+			t2.set_position(point.X,point.Y,point.Z)
+			i+=1
+
+		if (rs.IsCurveClosed(curve)==False): # if curve is not closed, lift pen
+			t.penup()
+			t2.penup()
+
+		t.set_position(inner_points[0].X,inner_points[0].Y,inner_points[0].Z) # close curve
+		t.set_position(inner_points[1].X,inner_points[1].Y,inner_points[0].Z) # close curve
+		t2.set_position(inner_points[0].X,inner_points[0].Y,inner_points[0].Z) # close curve
+		t2.set_position(inner_points[1].X,inner_points[1].Y,inner_points[0].Z) # close curve
+	return t2.get_lines()
+
+def find_closest_slice(slice,next_slices):
+	min_distance=10000
+	index=-1
+	# slice curve and pick an index for comparison point
+	points0 = rs.DivideCurve(slice, 20)
+	j = random.randint(4,16) #randomize the point that is chosen for comparison, avoiding the ends
+
+	for i in range (0,len(next_slices)):
+		# use a point along curve as a comparison to avoid branch comparison problems
+		closest=rs.CurveClosestPoint(next_slices[i],points0[j])
+		closest_point = rs.EvaluateCurve(next_slices[i], closest)
+		distance = abs(rs.Distance(points0[j],closest_point))
+		if (distance<min_distance):
+			min_distance=distance
+			index=i
+	return index
+
+
+# WeaveSlicer specific functions
 
 def find_points_and_angles_angles(slice0, slice1, period):
 	resolution = period/2.0
@@ -225,8 +381,6 @@ def weave_points_and_angles(t,points,points1,angles,slice0,slice1,wall_width=3.0
 		t3.set_position_point(points[0])
 	return t3.get_lines()
 
-
-
 def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers=0, glass_clay=False):
 	t.write_gcode_comment("**************************************************")
 	t.write_gcode_comment("******* file generated by WeaveSlicer ************")
@@ -241,6 +395,7 @@ def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers
 	layers = []
 	points=False
 	x=-1
+	bottom_layer=False
 	for i in range (0,len(shape_slices)-1):
 		k=0
 		while (k<len(shape_slices[i])):
@@ -248,6 +403,7 @@ def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers
 			slice0 = shape_slices[i][k] # current slice
 
 			if (i<bottom_layers and rs.IsCurveClosed(slice0)):
+				bottom_layer=[]
 				point = rs.CurveAreaCentroid(slice0)[0]
 				bottom_curve = slice0
 				if (mode==1):
@@ -256,10 +412,10 @@ def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers
 					bottom_curve = rs.OffsetCurve(slice0,point,wall_width*1.5)[0]
 				else:
 					bottom_curve = slice0
-				bottom_curves=get_bottom_offset_curves(bottom_curve,t.get_extrude_width())
-				follow_curve(t,bottom_curve)
+				bottom_curves=get_offset_curves(bottom_curve,t.get_extrude_width())
+				bottom_layer = bottom_layer + [follow_curve(t,bottom_curve)]
 				for c in range (0,len(bottom_curves)):
-					follow_curve(t,bottom_curves[c])
+					bottom_layer = bottom_layer + [follow_curve(t,bottom_curves[c])]
 					t.penup()
 
 			if (len(shape_slices[i])==1):
@@ -294,7 +450,10 @@ def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers
 			if (x==-1):
 				print("error at index: " +str(i))
 			else:
-				layers.append(x)
+				if (i<bottom_layers and bottom_layer):
+					layers.append(bottom_layer+[x])
+				else:
+					layers.append(x)
 			t.pendown()
 			k+=1
 			
@@ -314,66 +473,15 @@ def weave_slice_turtle (t,shape,wall_width=3.0,period=3.0, mode=1, bottom_layers
 def weave_slice (shape, file=False, layer_height=1.0, wall_width=3.0, period=3.0, mode=1, bottom_layers=0):
 	t = ExtruderTurtle()
 	if (file):
-		t.setup(filename=file, printer = "micro")
+		t.setup(filename=file, printer = "eazao")
 	else:
-		t.setup(printer = "micro")
+		t.setup(printer = "eazao")
 	t.write_gcode_comment("You can replace everything above this line with the correct header for your 3D printer.")
 	t.set_layer_height(layer_height)
 	t.penup()
 	weave_slice_turtle(t,shape,wall_width=wall_width,period=period, mode=mode, bottom_layers=bottom_layers)
 	t.finish()
 	return t.get_lines()
-
-
-		
-
-
-
-
-
-def follow_curve(t, curve, double_wall = False, inner_wall=False, steps=100):
-	points = rs.DivideCurve (curve, steps)
-	dtheta = 360.0/steps
-
-	i=0
-	if (inner_wall==False):
-		for point in points:
-			if (i==0):
-				t.set_position(point.X,point.Y,point.Z)
-				t.pendown()
-			else:
-				t.pendown()
-			t.set_position(point.X,point.Y,point.Z)
-			i+=1
-
-	if (rs.IsCurveClosed(curve)==False): # if curve is not closed, lift pen
-			t.penup()
-
-	t.set_position(points[0].X,points[0].Y,point.Z) # close curve
-	t.set_position(points[1].X,points[1].Y,point.Z) # close curve
-
-	if (double_wall or inner_wall):
-		inner_curve = rs.OffsetCurve(curve, [0,0,0], t.get_extrude_width())
-		print(inner_curve)
-		if (inner_curve):
-			inner_points = rs.DivideCurve (inner_curve, steps)
-		else:
-			#if offset fails, just return
-			print("Couldn't create an offset curve to follow.")
-			return()
-		i=0
-		for point in inner_points:
-			if (i==0):	
-				t.set_position(point.X,point.Y,point.Z)
-				t.pendown()
-			t.set_position(point.X,point.Y,point.Z)
-			i+=1
-
-		if (rs.IsCurveClosed(curve)==False): # if curve is not closed, lift pen
-			t.penup()
-
-		t.set_position(inner_points[0].X,inner_points[0].Y,inner_points[0].Z) # close curve
-		t.set_position(inner_points[1].X,inner_points[1].Y,inner_points[0].Z) # close curve
 
 def small_curve_check(slice, amplitude, mode):
 
@@ -396,131 +504,6 @@ def small_curve_check(slice, amplitude, mode):
 		return new_amplitude
 	else:
 		return amplitude
-
-def find_closest_slice(slice,next_slices):
-	min_distance=10000
-	index=-1
-	# slice curve and pick an index for comparison point
-	points0 = rs.DivideCurve(slice, 20)
-	j = random.randint(4,16) #randomize the point that is chosen for comparison, avoiding the ends
-
-	for i in range (0,len(next_slices)):
-		# use a point along curve as a comparison to avoid branch comparison problems
-		closest=rs.CurveClosestPoint(next_slices[i],points0[j])
-		closest_point = rs.EvaluateCurve(next_slices[i], closest)
-		distance = abs(rs.Distance(points0[j],closest_point))
-		if (distance<min_distance):
-			min_distance=distance
-			index=i
-	return index
-
-def check_and_follow_curves(t,curve_list,last_curve):
-	curve=last_curve
-	for i in range(0,len(curve_list)):
-		next_curve = curve_list[i]
-		if (next_curve):
-			try:
-				area=rs.CurveArea(curve)[0]
-				next_area=rs.CurveArea(next_curve)[0]
-				if (len(curve_list)==1 and area-next_area<t.get_extrude_width()):
-					#redundant curve, return and don't follow
-					return
-				else:
-					follow_curve(t, next_curve)
-					if(len(curve_list)>1):
-						t.penup()
-						t.lift(5)
-			except Exception as e:
-				print(e)
-				print("Bottom layer may be problematic")
-				# return
-		else:
-			break
-		curve=next_curve
-
-	return curve
-
-
-def get_bottom_offset_curves (curve,distance):
-
-	if (rs.IsCurveClosable(curve)==False):
-		print("Can't make a bottom for an open form.")
-		return
-
-	point = rs.CurveAreaCentroid(curve)[0]
-	next_curve=True
-	count=0
-
-	# while (next_curve and count<100):
-	# 	if(curve):
-	# 		next_curve_list=rs.OffsetCurve(curve, point, distance=distance,style=3)
-	# 		if (not(next_curve_list)):
-	# 			return
-	# 		curve = check_and_follow_curves(t,next_curve_list,curve)
-
-	# 	count+=1
-
-	curve_list=[]
-	curve_list=recursive_offset(curve,curve_list,distance)
-
-	new_curve_list=[]
-
-	#get rid of duplicate curves in list
-	for i in range(0,len(curve_list)):
-		flag=False
-		curve=curve_list[i]
-		start0 = rs.CurveStartPoint(curve)
-		for j in range(i+1,len(curve_list)):
-			start1 = rs.CurveStartPoint(curve_list[j])
-			if(rs.Distance(start0,start1)<distance/2.0):
-				flag=True
-				break
-
-		if (flag==False):
-			compare_point=[-1000,0,0]
-			closest=rs.CurveClosestPoint(curve_list[i], compare_point)
-			sort_point = rs.EvaluateCurve(curve_list[i], closest)
-			new_curve_list.append({"curve": curve_list[i], "x": sort_point.X})
-
-	new_curve_list.sort(key=lambda item: item["x"])
-
-	curves=[]
-	for i in range(len(new_curve_list)):
-		curves.append(new_curve_list[i]["curve"])
-
-	return curves
-
-def recursive_offset (curve,curve_list,distance):
-
-	if (rs.IsCurveClosable(curve)==False):
-		print("Can't make a bottom for an open form.")
-		return []
-
-	c=rs.CurveLength(curve)
-	point = rs.CurveAreaCentroid(curve)[0]
-
-	if (distance>c/10):
-		# print("offset end condition curve length")
-		return []
-
-	a = rs.CurveArea(curve)[0]
-	if (distance>math.sqrt(a/10)):
-		# print("offset end condition curve area")
-		return []
-
-	next_curve_list=rs.OffsetCurve(curve, point, distance=distance,style=3)
-	if (not(next_curve_list)):
-		# print("couldn't find any more offsets")
-		return []
-
-	for i in range (0,len(next_curve_list)):
-		# add current offset curve to list:
-		# find new offsets for current offset:
-		curve_list = curve_list + [next_curve_list[i]]+recursive_offset(next_curve_list[i],curve_list,distance)
-
-	return curve_list
-
-
 
 
 
